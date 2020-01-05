d4bb757b71d52813fc31b3d3560eb315885508bf
Dmitriy Cherchenko <dcherchenko@gmail.com>
2020-01-04 22:40:57 -0800

    plugin metadata updates

--- allow-svg-uploads.php
+++ allow-svg-uploads.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: Allow SVG Upload
-Plugin URI: https://widerwebs.com
+Plugin Name: Allow SVG Uploads
+Plugin URI: https://github.com/dchenk/allow-svg-uploads
 Description: Allow editors and administrators to upload SVG files.
 Version: 1.0
 Author: widerwebs.com
@@ -10,7 +10,7 @@ License: GPL-2.0+
 */
 
 function ww_allow_svg_upload($mimes = []) {
-	if (current_user_can('edit_others_posts') {
+	if (current_user_can('edit_others_posts')) {
 		$mimes['svg'] = 'image/svg+xml';
 	}
 	return $mimes;
